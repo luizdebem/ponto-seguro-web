953576c5526b488359c1b266221cc60c4c88f108
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-05-31 19:51:40 -0300

    Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar /> 
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from '../../assets/logo.png';
+import Dashboard from '@material-ui/icons/Dashboard';
+import WarningOutlined from '@material-ui/icons/WarningOutlined';
+import Store from '@material-ui/icons/Store'
+import './Sidebar.css';
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container">
+      <img src={logo} alt="Ponto Seguro Logo"></img>
+
+      <div className="items-container">
+        <div className="menu-item">
+          <Dashboard />
+          <span>Dashboard</span>
+        </div>
+        <div className="menu-item">
+          <WarningOutlined />
+          <span>Ocorrências</span>
+        </div>
+        <div className="menu-item">
+          <Store />
+          <span>Estabelecimentos</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
