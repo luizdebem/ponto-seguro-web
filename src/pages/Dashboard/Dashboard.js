78bcc94b4c61465020b4e04f1ae3f73adb56c411
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-06-01 09:41:36 -0300

    Dashboard page done

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import './Dashboard.css'
 import Settings from '@material-ui/icons/Settings'
 import PowerSettingsNew from '@material-ui/icons/PowerSettingsNew'
 import Search from '@material-ui/icons/Search'
+import AgressionChart from '../../assets/agressions-graph.png'
+import RobberyChart from '../../assets/robbery-graph.png'
 
 const Dashboard = () => {
   return (
@@ -10,7 +12,7 @@ const Dashboard = () => {
       <header className="dashboard-header">
         <div className="search-container">
           <Search className="search-icon" />
-          <input class="search-input" type="text" placeholder="Buscar no sistema" />
+          <input className="search-input" type="text" placeholder="Buscar no sistema" />
         </div>
 
         <span className="settings">
@@ -32,6 +34,47 @@ const Dashboard = () => {
           </div>
         </div>
       </header>
+
+      <main className="main-content">
+        <div className="welcome">
+          <h1>Olá, Luiz Guilherme</h1>
+          <p>Estas são as informações de segurança pública</p>
+        </div>
+
+        <div className="information-container">
+          <div className="red box">
+            <span><b>Furtos</b></span>
+            <span>Total</span>
+            <div>
+              <span>1896</span>
+            </div>
+          </div>
+
+          <div className="blue box">
+            <span><b>Agressões</b></span>
+            <span>Total</span>
+            <div>
+              <span>568</span>
+            </div>
+          </div>
+
+          <div className="green box">
+            <span><b>Roubos</b></span>
+            <span>Total</span>
+            <div>
+              <span>139</span>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="charts-container">
+          <img src={RobberyChart} alt="Gráfico de Roubos e Furtos"></img>
+          <img src={AgressionChart} alt="Gráfico de Agressões"></img>
+        </div>
+      </main>
+
+
     </div>
   )
 }
