a88c4f0bcafd16d416f5ab5a05270a8fcb47f286
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-06-03 15:36:24 -0300

    WIP - Reports screen, added table with database data

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,22 +1,48 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
-import {ReportService} from '../../services/ReportService';
+import { ReportService } from '../../services/ReportService';
+import './Reports.css';
+import moment from 'moment';
 
 const Reports = () => {
+  const [reports, setReports] = useState([]);
 
-  const listReports = async () => {
+  const fetchReports = async () => {
     const res = await ReportService.getReports();
-    console.log(res);
+    const reports = res.data.data;
+    console.log(reports);
+    setReports(reports);
   }
 
   useEffect(() => {
-    listReports();
-    console.log("Reports");
+    fetchReports();
   }, [])
 
   return (
-    <div>
-      Reports page
+    <div className="table-container">
+      <p>OCORRÊNCIAS</p>
+      {reports.length > 0 && <table>
+        <tr className="table-head">
+          <th>OC.</th>
+          <th>Cidade</th>
+          <th>Logradouro</th>
+          <th>Tipo</th>
+          <th>Data</th>
+          <th>Horário</th>
+          <th>Detalhes</th>
+        </tr>
+        {reports.map((report, index) => (
+          <tr className="table-body">
+            <td>{index + 1}</td>
+            <td>Florianópolis - SC</td>
+            <td>Rua Frei Caneca</td>
+            <td>{report.userType === 'PEDESTRIAN' ? 'Pedestre' : 'Motorista'}</td>
+            <td>{moment(report.when).format('L')}</td>
+            <td>{moment(report.when).format('LT')}</td>
+            <td><button>DETALHES</button></td>
+          </tr>
+        ))}
+      </table>}
     </div>
   )
 }
